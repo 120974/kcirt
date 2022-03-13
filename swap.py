0f049bb8b051c419992752163911e772d19e02ba
120974 <69763056+120974@users.noreply.github.com>
2022-03-12 19:22:10 -0500

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,20 @@
+def swapFileData():
+
+    print("")
+    
+    file1 = input("Enter 1st file name - ")
+    file2 = input("Enter 2nd file name - ")
+
+    ref1 = open(file1, 'r+')
+    fileOneData = ref1.read()
+
+    ref2 = open(file2, 'r+')
+    fileTwoData = ref2.read()
+
+    write1 = open(file1, 'w+')
+    write1.write(fileTwoData)
+
+    write2 = open(file2, 'w+')
+    write2.write(fileOneData)
+
+swapFileData()
